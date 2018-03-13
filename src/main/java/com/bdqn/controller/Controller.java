956faeb16aa91d924de9fe79edbae588bb0a7a77
zhu7522025 <1004918000@qq.com>
2018-03-13 10:39:03 +0800

    2018.3.13

---
+++ src/main/java/com/bdqn/controller/Controller.java
@@ -0,0 +1,50 @@
+package com.bdqn.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.bdqn.entity.Product;
+import com.bdqn.service.ProductService;
+import com.bdqn.util.Message;
+import com.github.pagehelper.PageInfo;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by 朱博文 on 2018/3/13.
+ */
+@org.springframework.stereotype.Controller
+@RequestMapping("product")
+public class Controller {
+    @Resource
+    private ProductService productService;
+
+    @ResponseBody
+    @RequestMapping(value = "updateproduct",method = RequestMethod.GET,produces = {"application/json;charset=utf-8"})
+    public String updateProduct(Product product){
+        int n= productService.updateProduct(product);
+        if(n>0){
+            return JSON.toJSONString(Message.success());
+
+        }
+        return JSON.toJSONString(Message.error());
+
+    }
+
+    @RequestMapping(value = "toProduct")
+    public String toProduct(Integer pageNum, Integer pageSiz, Model model){
+        PageInfo<Product> pageInfo=productService.queryAll(pageSiz,pageNum);
+        model.addAttribute("pageInfo",pageInfo);
+        return "product";
+
+    }
+    @ResponseBody
+    @RequestMapping(value = "queryProductById",method =RequestMethod.GET,produces = {"application/json;charset=utf-8"})
+    public String queryProductById(int id){
+        Product product=productService.queryProductById(id);
+        return JSON.toJSONString(product);
+
+    }
+}

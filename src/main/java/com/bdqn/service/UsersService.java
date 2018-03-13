9a668b58dc020b33809482ffdb7ecd15060bee85
httmx <13811007055@139.com>
2018-03-13 10:44:17 +0800

    20180313更新users:entity,dao,impl,controller;jsp:index,main

---
+++ src/main/java/com/bdqn/service/UsersService.java
@@ -0,0 +1,10 @@
+package com.bdqn.service;
+
+import com.bdqn.entity.Users;
+
+/**
+ * Created by Max on 3-13-2018-013.
+ */
+public interface UsersService {
+    public Users UsersLogin(Users users);
+}

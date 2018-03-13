8c74a3cda4e143fef877751167a678674efacd4d
zhu7522025 <1004918000@qq.com>
2018-03-13 10:53:23 +0800

    2018.3.13.10.53

--- src/main/java/com/bdqn/entity/Product.java
+++ src/main/java/com/bdqn/entity/Product.java
@@ -6,4 +6,37 @@ package com.bdqn.entity;
 public class Product {
     private int id,quantity;
     private String productName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", quantity=" + quantity +
+                ", productName='" + productName + '\'' +
+                '}';
+    }
 }
